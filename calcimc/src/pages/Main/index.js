4a7037f64f9ba5b4ea10382854889dfd809c5c7a
fuinho <erickalexandrex@gmail.com>
2019-06-03 18:13:31 -0300

    implementacao da logica e finalizacao do app

--- calcimc/src/pages/Main/index.js
+++ calcimc/src/pages/Main/index.js
@@ -31,18 +31,81 @@ const styles = StyleSheet.create({
     color: '#f2eded',
     fontWeight: 'bold',
   },
+  resultado: {
+    alignSelf: 'center',
+    color: 'lightgray',
+    fontSize: 65,
+    padding: 15,
+  },
 });
 
-const Main = () => (
-  <View style={styles.container}>
-    <View style={styles.entradas}>
-      <TextInput placeholder="Altura" keyboardType="numeric" style={styles.input} />
-      <TextInput placeholder="Peso" keyboardType="numeric" style={styles.input} />
-    </View>
-    <TouchableOpacity style={styles.button} onPress={() => {}}>
-      <Text style={styles.buttonText}>Calcular</Text> 
-    </TouchableOpacity>
-  </View>
-);
+// const Main = () => (
 
-export default Main;
+//   <View style={styles.container}>
+//     <View style={styles.entradas}>
+//       <TextInput placeholder="Altura" keyboardType="numeric" style={styles.input} />
+//       <TextInput placeholder="Peso" keyboardType="numeric" style={styles.input} />
+//     </View>
+//     <TouchableOpacity style={styles.button} onPress={() => {}}>
+//       <Text style={styles.buttonText}>Calcular</Text> 
+//     </TouchableOpacity>
+//     <Text style={styles.resultado}>{this.state.resultado}</Text>
+//     <Text style={[styles.resultado, { fontSize: 40 }]}>{this.state.resultadoText}</Text>
+//   </View>
+// );
+
+
+type Props = {};
+export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      altura: 0, massa: 0, resultado: 0, resultadoText: '',
+    };
+    this.calcular = this.calcular.bind(this);
+  }
+
+  calcular() {
+    const imc = this.state.massa / (this.state.altura * this.state.altura)
+    const s = this.state;
+    s.resultado = imc;
+
+    if (s.resultado < 16) {
+      s.resultadoText = 'Magreza Grave';
+    } else if (s.resultado < 17) {
+      s.resultadoText = 'Magreza Moderada';
+    } else if (s.resultado < 18.5) {
+      s.resultadoText = 'Magreza Leve';
+    } else if (s.resultado < 25) {
+      s.resultadoText = 'Saudável';
+    } else if (s.resultado < 30) {
+      s.resultadoText = 'Sobrepeso';
+    } else if (s.resultado < 35) {
+      s.resultadoText = 'Obesidade Grau I';
+    } else if (s.resultado < 40) {
+      s.resultadoText = 'Obesidade Grau II (Severa)';
+    } else {
+      s.resultadoText = 'Obesidade Grau III (Mórbida)';
+    }
+    this.setState(s);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.entradas}>
+          <TextInput placeholder="Altura" keyboardType="numeric" style={styles.input} onChangeText={(altura) => { this.setState({ altura }); }} />
+          <TextInput placeholder="Peso" keyboardType="numeric" style={styles.input} onChangeText={(massa) => { this.setState({ massa }); }} />
+        </View>
+        <TouchableOpacity style={styles.button} onPress={this.calcular}>
+          <Text style={styles.buttonText}>Calcular</Text>
+        </TouchableOpacity>
+        <Text style={styles.resultado}> {this.state.resultado.toFixed(2)} </Text>
+        <Text style={[styles.resultado, { fontSize: 40 }]}>{this.state.resultadoText}</Text>
+      </View>
+    );
+  }
+}
+
+
+// export default Main;
